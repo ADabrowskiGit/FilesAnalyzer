44ab71a2a9dc2dcac72e3e0c7547d7d7772f9bf7
Adam <adam@adam.pc>
2018-06-27 12:21:38 +0200

    Initial commit.

---
+++ FilesAnalyzer/FilesAnalyzer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilesAnalyzer.Startup))]
+namespace FilesAnalyzer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
