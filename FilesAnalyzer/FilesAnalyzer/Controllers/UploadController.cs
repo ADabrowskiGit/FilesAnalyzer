418643ead2b9f076a4b9a2801c502d0fe8f67682
Adam <adam@adam.pc>
2018-06-27 13:36:12 +0200

    Added: file upload, save file on server, add file record to DB

---
+++ FilesAnalyzer/FilesAnalyzer/Controllers/UploadController.cs
@@ -0,0 +1,50 @@
+﻿using FilesAnalyzer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilesAnalyzer.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View("Upload");
+        }
+
+        //[HttpGet]
+        //public ActionResult Upload()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    string[] fileNameSplit = fileName.Split('.');
+                    var myUniqueFileName = string.Format(@"{0}.{1}", DateTime.Now.Ticks, fileNameSplit[1]);
+                    var path = Path.Combine(Server.MapPath("~/Files/"), myUniqueFileName);//TO DO change path
+                    file.SaveAs(path);
+                    if ( new FDBFileRepository().AddFileRecord("author", "description", fileNameSplit[1], fileNameSplit[0], myUniqueFileName, file.ContentLength, "title"))
+                    {//succesfully uploaded
+                        return RedirectToAction("UploadDocument");
+                    }
+
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FilesAnalyzer/FilesAnalyzer/Repositories/FDBFileRepository.cs
@@ -0,0 +1,40 @@
+﻿using FilesAnalyzer.DBModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilesAnalyzer.Repositories
+{
+    public class FDBFileRepository
+    {
+        public bool AddFileRecord(string author, string description, string extension, string originalName, string serverName, int size, string title)
+        {
+            try
+            {
+                FDBFile fDBFile = new FDBFile();
+                fDBFile.Author = author;
+                fDBFile.Description = description;
+                fDBFile.Extension = extension;
+                fDBFile.IsDeleted = false;
+                fDBFile.OriginalName = originalName;
+                fDBFile.ServerName = serverName;
+                fDBFile.Size = size;
+                fDBFile.Title = title;
+
+                using (var context = new FDBEntities())
+                {
+                    context.FDBFile.Add(fDBFile);
+                    context.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //TO DO exception to handle
+                return false;
+            }
+
+        }
+    }
+}
