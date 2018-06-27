3652706cca52cd8be61bfe2a4e4740fdaf2d25d7
Adam <adam@adam.pc>
2018-06-27 17:59:19 +0200

    Add creating indexes, some searching and displaying

--- FilesAnalyzer/FilesAnalyzer/Controllers/UploadController.cs
+++ FilesAnalyzer/FilesAnalyzer/Controllers/UploadController.cs
@@ -1,11 +1,20 @@
-﻿using FilesAnalyzer.Repositories;
+﻿using FilesAnalyzer.Helpers;
+using FilesAnalyzer.Repositories;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.HtmlControls;
 using TikaOnDotNet.TextExtraction;
+using static javax.measure.unit.Dimension;
 
 namespace FilesAnalyzer.Controllers
 {
@@ -17,11 +26,7 @@ namespace FilesAnalyzer.Controllers
             return View("Upload");
         }
 
-        //[HttpGet]
-        //public ActionResult Upload()
-        //{
-        //    return View();
-        //}
+
 
         [HttpPost]
         public ActionResult Upload()
@@ -57,16 +62,55 @@ namespace FilesAnalyzer.Controllers
                         {
                             description = "";
                         }
+
+                        Analyzer analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29);
+                        Lucene.Net.Store.Directory directory = FSDirectory.Open(@"C:\Users\Totalit\Source\Repos\FilesAnalyzer\FilesAnalyzer\FilesAnalyzer\IndexedFiles");
+                        IndexWriter writer = new IndexWriter(directory, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED);
+                        Document doc = new Document();
+
+                        doc.Add(new Field("Author",
+                        author,
+                        Field.Store.YES,
+                        Field.Index.ANALYZED));
+                        doc.Add(new Field("title",
+                        title,
+                        Field.Store.YES,
+                        Field.Index.ANALYZED));
+                        doc.Add(new Field("description",
+                        description,
+                        Field.Store.YES,
+                        Field.Index.ANALYZED));
+                        doc.Add(new Field("text",
+                        wordDocContents.Text,
+                        Field.Store.YES,
+                        Field.Index.ANALYZED));
+
+
+                        writer.AddDocument(doc);
+                        writer.Optimize();
+                        writer.Close();
+
                     }
-                    if (new FDBFileRepository().AddFileRecord(author, description, fileNameSplit[1], fileNameSplit[0], myUniqueFileName, file.ContentLength, title))
-                    {//succesfully uploaded
-                        return RedirectToAction("UploadDocument");
+                    else
+                    {
+                        // TO DO html version
                     }
+
 
                 }
             }
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetSearchData(string tbSearchValue)
+        {
+
+            List<SampleData> searchresult = new LuceneSearch().Search(tbSearchValue).ToList();
+
+
+            return Json(new { Url = Url.Action("List", searchresult) });
         }
     }
 }
--- FilesAnalyzer/FilesAnalyzer/Helpers/ExtractHelper.cs
+++ FilesAnalyzer/FilesAnalyzer/Helpers/ExtractHelper.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +13,13 @@ using System.Web;
 
 namespace FilesAnalyzer.Helpers
 {
+    public class SampleData
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+
     public class ExtractHelper
     {
 
@@ -21,6 +34,95 @@ namespace FilesAnalyzer.Helpers
                 //TO DO Exception handle
                 return "";
             }
+        }
+
+
+    }
+
+
+    public class LuceneSearch
+    {
+        private static Query parseQuery(string searchQuery, QueryParser parser)
+        {
+            Query query;
+            try
+            {
+                query = parser.Parse(searchQuery.Trim());
+            }
+            catch (ParseException)
+            {
+                query = parser.Parse(QueryParser.Escape(searchQuery.Trim()));
+            }
+            return query;
+        }
+
+        private SampleData _mapLuceneDocumentToData(Document doc)
+        {
+            return new SampleData
+            {
+                Title = doc.Get("title"),
+                Author = doc.Get("Author"),
+                Description = doc.Get("description")
+            };
+        }
+        private IEnumerable<SampleData> _mapLuceneToDataList(IEnumerable<Document> hits)
+        {
+            return hits.Select(_mapLuceneDocumentToData).ToList();
+        }
+        private IEnumerable<SampleData> _mapLuceneToDataList(IEnumerable<ScoreDoc> hits,
+            IndexSearcher searcher)
+        {
+            return hits.Select(hit => _mapLuceneDocumentToData(searcher.Doc(hit.Doc))).ToList();
+        }
+
+        private IEnumerable<SampleData> _search(string searchQuery)
+        {
+            // validation
+            if (string.IsNullOrEmpty(searchQuery.Replace("*", "").Replace("?", ""))) return new List<SampleData>();
+
+            // set up lucene searcher
+            using (var searcher = new IndexSearcher(FSDirectory.Open(@"C:\Users\Totalit\Source\Repos\FilesAnalyzer\FilesAnalyzer\FilesAnalyzer\IndexedFiles"), false))
+            {
+                var hits_limit = 1000;
+                var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+
+
+                var parser = new MultiFieldQueryParser
+                    (Lucene.Net.Util.Version.LUCENE_30, new[] { "Author", "title", "description" }, analyzer);
+                var query = parseQuery(searchQuery, parser);
+                var hits = searcher.Search
+                (query, null, hits_limit, Sort.RELEVANCE).ScoreDocs;
+                var results = _mapLuceneToDataList(hits, searcher);
+                analyzer.Close();
+                searcher.Dispose();
+                return results;
+
+            }
+        }
+
+        public IEnumerable<SampleData> Search(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return new List<SampleData>();
+
+            var terms = input.Trim().Replace("-", " ").Split(' ')
+                .Where(x => !string.IsNullOrEmpty(x)).Select(x => x.Trim() + "*");
+            input = string.Join(" ", terms);
+
+            return _search(input);
+        }
+
+        public IEnumerable<SampleData> GetAllIndexRecords()
+        {
+
+            // set up lucene searcher
+            var searcher = new IndexSearcher(FSDirectory.Open(@"C:\Users\Totalit\Source\Repos\FilesAnalyzer\FilesAnalyzer\FilesAnalyzer\IndexedFiles"), false);
+            var reader = IndexReader.Open(FSDirectory.Open(@"C:\Users\Totalit\Source\Repos\FilesAnalyzer\FilesAnalyzer\FilesAnalyzer\IndexedFiles"), false);
+            var docs = new List<Document>();
+            var term = reader.TermDocs();
+            while (term.Next()) docs.Add(searcher.Doc(term.Doc));
+            reader.Dispose();
+            searcher.Dispose();
+            return _mapLuceneToDataList(docs);
         }
     }
 }
